c1077bcf3aae06df048b1ec5c402ed6236c6b278
ajmol71 <71am14@gmail.com>
2025-03-07 17:46:50 -0800

    distilbert_and_test_1

--- Model_Tests.py
+++ Model_Tests.py
@@ -1,10 +1,12 @@
-# import huggingface_hub
-# import transformers
-# from mpmath.libmp import dps_to_prec
-# from transformers import TFDistilBertForQuestionAnswering, TrainingArguments, pipeline, AutoTokenizer, TFAutoModelForSequenceClassification, DistilBertTokenizer
-# import evaluate
+import huggingface_hub
+import transformers
+from mpmath.libmp import dps_to_prec
+from transformers import TFDistilBertForQuestionAnswering, TrainingArguments, pipeline, AutoTokenizer, TFAutoModelForSequenceClassification, DistilBertTokenizer
+import evaluate
 
 import pandas as pd
+import csv
+import random
 
 import tensorflow as tf
 
@@ -44,3 +46,45 @@ import tensorflow as tf
 
 
 # LOAD DATA
+and_qs = pd.read_csv("AnD_questions.csv")
+and_header = ["model", "q_num", "q_type", "response", "correct"]
+
+d_tokenizer = DistilBertTokenizer.from_pretrained("distilbert-base-cased-distilled-squad")
+dist = TFDistilBertForQuestionAnswering.from_pretrained("distilbert-base-cased-distilled-squad")
+
+
+def test_model(tokenizer, model, test_data):
+    rows_list = []
+    for index, row in test_data.iterrows():
+        prompt, info = row["question"], row["prompt"]
+
+
+        inputs = tokenizer(prompt, info, return_tensors="tf")
+        outputs = model(**inputs)
+
+        answer_start_index = int(tf.math.argmax(outputs.start_logits, axis=-1)[0])
+        answer_end_index = int(tf.math.argmax(outputs.end_logits, axis=-1)[0])
+
+        predict_answer_tokens = inputs.input_ids[0, answer_start_index : answer_end_index + 1]
+        answer = tokenizer.decode(predict_answer_tokens)
+
+        if row['correct_a'] in answer:
+            correct = 1
+        else:
+            correct = 0
+
+        print("\nQUESTION: ", row['question'])
+        print(answer)
+
+        q_dict = {"model": model.name, "q_num": row["q_num"], "q_type": row["q_type"], "answer": answer, "correct": correct}
+
+        rows_list.append(q_dict)
+    pd_df = pd.DataFrame(rows_list)
+    return pd_df
+
+answer_df = test_model(d_tokenizer, dist, and_qs)
+
+answer_df.to_csv("AnD_qs_performance.csv")
+
+print()
+print(answer_df.head())
